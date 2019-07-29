d3ddec41a695b2d3c740b76df4a38eb70f456058
moonlightL <jx8996@163.com>
2019-07-29 08:52:58 +0800

    修改

---
+++ freedom-boot-common/src/main/java/com/extlight/common/exception/GlobalExceptionMap.java
@@ -0,0 +1,11 @@
+package com.extlight.common.exception;
+
+/**
+ * @Author moonlight
+ * @ClassName: GlobalExceptionMap
+ * @ProjectName freedom-boot
+ * @Description: TODO
+ * @Date 2019-07-29 01:07
+ */
+public class GlobalExceptionMap {
+}
